236be46d38c57661c48f053f0fd7fa8d9d792730
wangzhiwei8 <wangzhiwei8@jd.com>
2018-06-22 16:21:06 +0800

    webpack多页应用

---
+++ build/webpack.dev.conf.js
@@ -0,0 +1,145 @@
+'use strict'
+var glob = require('glob');
+const utils = require('./utils')
+const webpack = require('webpack')
+const config = require('../config')
+const merge = require('webpack-merge')
+const path = require('path')
+const baseWebpackConfig = require('./webpack.base.conf')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
+const portfinder = require('portfinder')
+
+const HOST = process.env.HOST
+const PORT = process.env.PORT && Number(process.env.PORT)
+
+const devWebpackConfig = merge(baseWebpackConfig, {
+  module: {
+    rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap, usePostCSS: true })
+  },
+  // cheap-module-eval-source-map is faster for development
+  devtool: config.dev.devtool,
+
+  // these devServer options should be customized in /config/index.js
+  devServer: {
+    clientLogLevel: 'warning',
+    historyApiFallback: {
+      rewrites: [
+        { from: /.*/, to: path.posix.join(config.dev.assetsPublicPath, 'index.html') },
+      ],
+    },
+    hot: true,
+    contentBase: false, // since we use CopyWebpackPlugin.
+    compress: true,
+    host: HOST || config.dev.host,
+    port: PORT || config.dev.port,
+    open: config.dev.autoOpenBrowser,
+    overlay: config.dev.errorOverlay
+      ? { warnings: false, errors: true }
+      : false,
+    publicPath: config.dev.assetsPublicPath,
+    proxy: config.dev.proxyTable,
+    quiet: true, // necessary for FriendlyErrorsPlugin
+    watchOptions: {
+      poll: config.dev.poll,
+    }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': require('../config/dev.env')
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(), // HMR shows correct file names in console on update.
+    new webpack.NoEmitOnErrorsPlugin(),
+    // https://github.com/ampedandwired/html-webpack-plugin
+    // new HtmlWebpackPlugin({
+    //   filename: 'index.html',
+    //   template: 'index.html',
+    //   inject: true
+    // }),
+    // copy custom static assets
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, '../static'),
+        to: config.dev.assetsSubDirectory,
+        ignore: ['.*']
+      }
+    ])
+  ]
+})
+
+
+// 获取指定路径下的入口文件
+function getEntries(globPath) {
+     var files = glob.sync(globPath),
+       entries = {};
+    // console.log(files);
+     files.forEach(function(filepath) {
+         // 取倒数第二层(view下面的文件夹)做包名
+         var split = filepath.split('/');
+         // console.log(split);
+         var name = split[split.length - 1].replace('.js','');
+
+         entries[name] = './' + filepath;
+     });
+
+     return entries;
+}
+  
+   
+var entries = getEntries('src/entry/*');
+console.log(entries);
+
+Object.keys(entries).forEach(function(name) {
+    // 每个页面生成一个entry，如果需要HotUpdate，在这里修改entry
+    devWebpackConfig.entry[name] = entries[name];
+    
+    // 每个页面生成一个html
+    var plugin = new HtmlWebpackPlugin({
+        // 生成出来的html文件名
+        filename: name + '.html',
+        // 每个html的模版，这里多个页面使用同一个模版
+        template: './build/template.html',
+        // 自动将引用插入html
+        inject: true,
+        // 每个html引用的js模块，也可以在这里加上vendor等公用模块
+        chunks: [name,'app','vendor','manifest'],
+        // minify: {
+        //   removeComments: true,
+        //   collapseWhitespace: true,
+        //   removeAttributeQuotes: true
+        //   // more options:
+        //   // https://github.com/kangax/html-minifier#options-quick-reference
+        // },
+        // necessary to consistently work with multiple chunks via CommonsChunkPlugin
+        chunksSortMode: 'dependency'
+    });
+    devWebpackConfig.plugins.push(plugin);
+})
+
+module.exports = new Promise((resolve, reject) => {
+  portfinder.basePort = process.env.PORT || config.dev.port
+  portfinder.getPort((err, port) => {
+    if (err) {
+      reject(err)
+    } else {
+      // publish the new Port, necessary for e2e tests
+      process.env.PORT = port
+      // add port to devServer config
+      devWebpackConfig.devServer.port = port
+
+      // Add FriendlyErrorsPlugin
+      devWebpackConfig.plugins.push(new FriendlyErrorsPlugin({
+        compilationSuccessInfo: {
+          messages: [`Your application is running here: http://${devWebpackConfig.devServer.host}:${port}`],
+        },
+        onErrors: config.dev.notifyOnErrors
+        ? utils.createNotifierCallback()
+        : undefined
+      }))
+
+      resolve(devWebpackConfig)
+    }
+  })
+})
